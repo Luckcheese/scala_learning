431381561d765c495e78f7ea965fc0eb62638cdb
Rafael Duarte Lucchesi <rafael.lucchesi@braspay.co>
2014-05-08 13:27:34 -0300

    Renamed week1 assignment folder to 01-recfun

--- 01-recfun/src/main/scala/recfun/Main.scala
+++ 01-recfun/src/main/scala/recfun/Main.scala
@@ -24,8 +24,8 @@ object Main {
   def balance(chars: List[Char]): Boolean = {
     def balance(chars: List[Char], count: Int): Int = {
       if (chars.isEmpty || count < 0) count
-      else if (chars.head == "(") balance(chars.tail, count + 1)
-      else if (chars.head == ")") balance(chars.tail, count - 1)
+      else if (chars.head == '(') balance(chars.tail, count + 1)
+      else if (chars.head == ')') balance(chars.tail, count - 1)
       else balance(chars.tail, count)
     }
     balance(chars, 0) == 0
